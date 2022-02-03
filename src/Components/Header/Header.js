e72a6561b86fe68325b7b60c03a197790866b114
Delilah Necrason <delilahnecrason@gmail.com>
2022-02-03 18:28:11 -0500

    Add links to nav bar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,17 @@
 import './Header.scss'
+import {NavLink} from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="header">
-      Header!
-
+      <NavLink to="/">
+        <p>Header!</p>
+      </ NavLink>
+      <section className="nav-bar">
+        <NavLink className="nav-link">About Us</NavLink>
+        <NavLink className="nav-link">Log In</NavLink>
+        <NavLink className="nav-link"><button>Sign Up</button></NavLink>
+      </section>
 
     </header>
   )
