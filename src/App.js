04f6907832d76a447915dacfcaf185a3ac36702f
Regan Losey <regan.losey@gmail.com>
2022-02-03 15:21:01 -0700

    Add button to link to schedule component

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ const App = () => {
         <Route path="/budget" element={<Budget />} />
         <Route path="/share" element={<Share />} />
       </Routes>
-
     </main>
   )
 
--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,8 +1,22 @@
 import './Homepage.scss'
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
   return (
-    <h1>Homepage!</h1>
+    <div className="homepage">
+      <section className="welcome-txt">
+        <h1>Free Tomorrow?</h1>
+        <h2>An app that helps you get your travel plans out of the air and onto the ground.</h2>
+      </section>
+
+      <div className="btn-container">
+        <Link to="/schedule" className="start-link">
+          <button className="start-btn"> Click Here To Get Started</button>
+        </Link>
+
+
+      </div>
+    </div>
   )
 }
 
