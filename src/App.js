bcad7e8e8d3070cd3d232c025acdcf61f5517f58
Regan Losey <regan.losey@gmail.com>
2022-02-03 15:08:51 -0700

    Hook up routes to components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Header from './Components/Header/Header';
 import Homepage from './Components/Homepage/Homepage';
 import Schedule from './Components/Schedule/Schedule';
 import Budget from './Components/Budget/Budget';
 import Share from './Components/Share/Share';
-
 import './App.scss';
 
 const App = () => {
-return (
-  <main className="App">
+  return (
+    <main className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/budget" element={<Budget />} />
+        <Route path="/share" element={<Share />} />
+      </Routes>
 
-
-  </main>
-)
+    </main>
+  )
 
 }
 
