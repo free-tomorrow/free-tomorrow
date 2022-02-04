565e5b25b356103b441b2e785a98f1ab46b265a8
Regan Losey <regan.losey@gmail.com>
2022-02-04 15:23:54 -0700

    Add container inside of homepage component

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 const Homepage = () => {
   return (
     <div className="homepage">
+      <div className="homepage-content" >
+
       <section className="welcome-txt">
         <h1>Free Tomorrow?</h1>
         <h2>An app that helps you get your travel plans out of the air and onto the ground.</h2>
@@ -13,6 +15,7 @@ const Homepage = () => {
         <Link to="/schedule" className="start-link" style={{width: "fit-content"}}>
           <button className="start-btn">Get Started</button>
         </Link>
+      </div>
       </div>
 
       
