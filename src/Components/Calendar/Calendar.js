5e1f93d3eb6fb853a62d19d567a78a2b218298c1
Regan Losey <regan.losey@gmail.com>
2022-02-04 21:28:56 -0700

    Added basic styles to calendar and changed font

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './Calendar.scss';
-import { Calendar as DatePicker } from 'react-calendar';
+// import React from 'react';
+// import './Calendar.scss';
+// import 'react-calendar/dist/Calendar.css'
+// import { Calendar as DatePicker } from 'react-calendar';
+// const Calendar = () => {
 
-const Calendar = () => {
-  
-  return (
-    <div className="date-picker-container">
-      <DatePicker className="date-picker" />
-    </div>
-  )
-}
+//   return (
+//     <div className="date-picker-container">
+//       <DatePicker className="date-picker" />
+//     </div>
+//   )
+// }
 
-export default Calendar;
+// export default Calendar;
