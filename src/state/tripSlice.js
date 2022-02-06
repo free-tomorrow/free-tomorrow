d153dcbd171a3e3b0942b11aaafb4a7561004379
Delilah Necrason <delilahnecrason@gmail.com>
2022-02-06 16:21:42 -0500

    Add trip slice mock data

--- src/state/tripSlice.js
+++ src/state/tripSlice.js
@@ -4,13 +4,23 @@ import { getDefaultNormalizer } from '@testing-library/react';
 
 export const tripSlice = createSlice({
   name: 'trips',
-  initialState: [{
-    id: 1,
-    travelers: [20],
-    invitedUsers: [],
-    confirmed: false,
-    proposedBudget: 500
-  }],
+  initialState: [
+    {
+      id: 1,
+      users: [20],
+      confirmed: false,
+      // toggles true when all users on the trip have confirmed their calendar & budget
+      proposed_budget: 500,
+      // PATCH request when a user changes budget (no request if budget isn't changed), POST when new trip is first made
+      available_dates: [
+        {
+          user_id: 1,
+          start_date: 1643950800000,
+          end_date: 1645246799999
+        }
+      ]
+    }
+  ],
   reducers: {
     addDates: (state, action) => {
       const newDateRange = {}
