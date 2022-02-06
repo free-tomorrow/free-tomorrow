9493d3b84a90894c7b44f753bbf0f83cd1d127fe
Delilah Necrason <delilahnecrason@gmail.com>
2022-02-06 16:20:25 -0500

    Edit deleteUser in userSlice

--- src/state/userSlice.js
+++ src/state/userSlice.js
@@ -4,12 +4,21 @@ import { createSlice } from '@reduxjs/toolkit'
 
 export const userSlice = createSlice({
   name: 'users',
-  initialState: [{
-    id: 1, 
-    name: 'Jordan',
-    email: '',
-    trips: []
-  }],
+  initialState: [
+    {
+      id: 20,
+      name: 'Delilah',
+      email: 'drnecrason@comcast.net',
+      trips: [
+        {
+          trip_id: 1,
+          created_by: 'Delilah',
+          is_verified: true
+          // toggles true when user has filled out their calendar & budget for the trip
+        }
+      ]
+    }
+  ],
   reducers: {
     addUser: (state, action) => {
       const newPerson = {
@@ -20,10 +29,10 @@ export const userSlice = createSlice({
       }
       state.push(newPerson)
     },
-    // deleteUser: (state, action) => {
-    //   const foundUser = state.find(user => user.name === action.payload.name) 
-    //   state.pop(foundUser)
-    // }
+    deleteUser: (state, action) => {
+      const foundUser = state.find(user => user.name === action.payload.name) 
+      state.splice(state.indexOf(foundUser), 1)
+    }
   }
 })
 
