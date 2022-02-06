e4996bec6ffc3faddac0df64e50c0e46ab37f076
Delilah Necrason <delilahnecrason@gmail.com>
2022-02-06 16:14:57 -0500

    Edit mock data file

--- src/mockData.js
+++ src/mockData.js
@@ -24,7 +24,7 @@ export const trips = [
     // PATCH request when a user changes budget (no request if budget isn't changed), POST when new trip is first made
     available_dates: [
       {
-        user_id: 1,
+        user_id: 20,
         start_date: 1643950800000,
         end_date: 1645246799999
       }
