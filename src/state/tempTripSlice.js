ae2f468b15a275b836d971caa2cee11171e19360
Delilah Necrason <delilahnecrason@gmail.com>
2022-02-06 21:34:05 -0500

    Add reducer for budget to tempTripSlice

--- src/state/tempTripSlice.js
+++ src/state/tempTripSlice.js
@@ -5,13 +5,11 @@ export const tempTripSlice = createSlice({
   initialState: {},
   reducers: {
     addDates: (state, action) => {
-      console.log(action.payload)
-      // const dateRange = {startDate: action.payload.startDate, endDate: action.payload.endDate}
-
       state.startDate = action.payload.startDate;
       state.endDate = action.payload.endDate;
-      
-      console.log(state)
+    },
+    addBudget: (state, action) => {
+      state.budget = action.payload;
     }
   }
 })
