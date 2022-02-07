07d8a7f03186fe9f0e2986cedb3d9f16e786f34e
Regan Losey <regan.losey@gmail.com>
2022-02-07 15:15:31 -0700

    Capture input from inputs in state

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,10 +1,43 @@
 import './SignUp.scss';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 
 const SignUp = () => {
-  return(
-    <h1>Sign Up!</h1>
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('')
+
+  return (
+    <div className="signup">
+      <div className="signup-content">
+        <article>
+          <h1> Welcome to Free Tomorrow!</h1>
+        </article>
+        <form className="signup-form">
+          <input
+            className="signup-input-name"
+            type="text"
+            maxLength="64"
+            name="name"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input 
+          className="signup-input email"
+          type="text"
+          maxLength="64"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          />
+
+
+        </form>
+
+
+      </div>
+
+
+    </div>
   )
 
 
