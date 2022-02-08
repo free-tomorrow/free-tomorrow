a72b85513060b58d03463ab6f84240e5fb87a852
Regan Losey <regan.losey@gmail.com>
2022-02-07 18:02:23 -0700

    Write basic DOM and url tests for Homepage

--- cypress/integration/Homepage_spec.js
+++ cypress/integration/Homepage_spec.js
@@ -13,4 +13,22 @@ describe('Homepage', () => {
     .get('h1').contains('Free Tomorrow?')
     .get('h2').contains('An app that helps you get your travel plans out of the air and onto the ground.')
   })
+
+  it('should have an image of travel aliens', () => {
+    cy.get('.aliens-img')
+    .should('have.attr', 'src', '/static/media/travel_aliens.23f539de23e82df94967.png')
+  })
+
+  it('should have a button to start the trip creation process', () => {
+    cy.get('.start-btn')
+    .contains('Let\'s get started')
+  })
+
+  it('should link to the schedule page on click of start button', () => {
+    cy.get('.start-btn')
+    .click()
+    cy.url('eq','http://localhost:3000/schedule')
+    cy.get('welcome-container')
+    .should('not.exist')
+  })
 })
