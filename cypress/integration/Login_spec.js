f462d852d7f438a2132ffaa65f7abaca0af812e4
Regan Losey <regan.losey@gmail.com>
2022-02-07 18:17:11 -0700

    Test for input typing and navigation back to /

--- cypress/integration/Login_spec.js
+++ cypress/integration/Login_spec.js
@@ -13,4 +13,19 @@ describe('Homepage', () => {
     .get('.login-form > h2')
     .contains('Let\'s get you logged in.')
   })
+
+  it('should allow the user to enter their email', () => {
+    cy.get('.login-form > .login-email-input')
+    .type('bobloblaw@loblaw.law')
+  })
+
+  //test here for the login button if we decide to have it redirect, display success or error message, etc.
+
+  it('should allow the user to navigate back to the homepage', () => {
+    cy.get('.logo')
+    .click()
+    .url('eq','http://localhost:3000/')
+    .get('.login-content')
+    .should('not.exist')
+  })
 })
