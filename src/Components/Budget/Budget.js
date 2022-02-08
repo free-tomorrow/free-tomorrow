988eb0b33bb0154780f3e2c4e4f47936228bddc9
Delilah Necrason <delilahnecrason@gmail.com>
2022-02-07 19:57:57 -0500

    Add link to share page

--- src/Components/Budget/Budget.js
+++ src/Components/Budget/Budget.js
@@ -1,6 +1,17 @@
 import './Budget.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Budget = () => {
+
+  const [budget, setBudget] = useState(0);
+
+  const createBudget = () => {
+    dispatch (
+
+    )
+  }
+
   return (
     <div className="budget-form">
       <div className="txt-container">
@@ -8,13 +19,16 @@ const Budget = () => {
         <p>No pressure on our end. Just pick whatever works for you.</p>
       </div>
       <div className="btn-container">
-        <button>I'm broke. Keep it under $500!</button>
-        <button>$500</button>
+        <button onClick={}>I'm broke. Keep it under $500!</button>
+        <button onClick={}>$500</button>
         <button>$1000</button>
         <button>$1500</button>
         <button>$2000</button>
         <button>The sky's the limit!</button>
       </div>
+      <Link to="/share">
+        <button>Continue</button>
+      </Link>
     </div>
   )
 }
