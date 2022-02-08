ef5b3f7d73b5194afc365836e91ec9e20d4fc7b7
Delilah Necrason <delilahnecrason@gmail.com>
2022-02-08 15:40:13 -0500

    Start testing schedule component

---
+++ cypress/integration/Schedule_spec.js
@@ -0,0 +1,11 @@
+describe('Schedule component tests', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('should have a dummy test', () => {
+    expect(true).to.eq(true)
+  })
+
+})
