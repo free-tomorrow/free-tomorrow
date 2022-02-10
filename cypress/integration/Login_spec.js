b284d9a60e2494a4d9bdf5ef55d9716c8b52119a
Regan Losey <regan.losey@gmail.com>
2022-02-10 15:26:54 -0700

    Add back homepage redirection test

--- cypress/integration/Login_spec.js
+++ cypress/integration/Login_spec.js
@@ -9,37 +9,37 @@ describe('Login page', () => {
 
   it('should greet the user and prompt them to log in', () => {
     cy.get('.login-content')
-    .contains('Good to see you again!')
-    .get('.login-form > h2')
-    .contains('Let\'s get you logged in.')
+      .contains('Good to see you again!')
+      .get('.login-form > h2')
+      .contains('Let\'s get you logged in.')
   })
 
 
   it('should allow the user to enter their email', () => {
     cy.get('.login-form > .login-email-input')
-    .type('bobloblaw@loblaw.law')
+      .type('bobloblaw@loblaw.law')
   })
 
   it('should not allow the user to login without a valid email format', () => {
     cy.get('.loginpg-btn')
-    .should('be.disabled').and('have.css', 'cursor', 'not-allowed')
-    .get('.login-email-input')
-    .type('bobloblaw@')
-    .get('.loginpg-btn')
-    .should('be.disabled')
-    .get('.login-email-input')
-    .type('loblaw.law')
-    .get('.loginpg-btn')
-    .should('not.be.disabled').and('have.css', 'cursor', 'pointer')
+      .should('be.disabled').and('have.css', 'cursor', 'not-allowed')
+      .get('.login-email-input')
+      .type('bobloblaw@')
+      .get('.loginpg-btn')
+      .should('be.disabled')
+      .get('.login-email-input')
+      .type('loblaw.law')
+      .get('.loginpg-btn')
+      .should('not.be.disabled').and('have.css', 'cursor', 'pointer')
   })
 
   //test here for the login button if we decide to have it redirect, display success or error message, etc.
 
-  // it('should allow the user to navigate back to the homepage', () => {
-  //   cy.get('.logo')
-  //   .click()
-  //   .url('eq','http://localhost:3000/')
-  //   .get('.login-content')
-  //   .should('not.exist')
-  // })
+  it('should allow the user to navigate back to the homepage', () => {
+    cy.get('.logo')
+    .click()
+    .url('eq','http://localhost:3000/')
+    .get('.login-content')
+    .should('not.exist')
+  })
 })
