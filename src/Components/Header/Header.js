a4553e742b9bef45cd5e46a253d5c3c2d631ca96
Delilah Necrason <delilahnecrason@gmail.com>
2022-02-12 19:27:29 -0500

    Create toggle function to log in/sign out

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,30 @@
 import './Header.scss'
 import {NavLink} from 'react-router-dom';
 import logo from '../../assets/co_logo.svg';
+import { useSelector } from 'react-redux';
 
 
 const Header = () => {
+  const state = useSelector((state) => state)
+
+  const toggleButton = () => {
+    // console.log(state.users)
+    // if(!state.users.id) {
+
+    // }
+
+
+    if(!state.users.id) {
+      return (
+        <NavLink to="/login" className="nav-link"><button className="login-btn">Log In</button></NavLink>
+      )
+    } else {
+      return (
+        <NavLink to="/" className="nav-link"><button className="signout-btn">Sign Out</button></NavLink>
+      )
+    }
+  }
+
   return (
     <header className="header">
       <div className="logo-container">
@@ -13,7 +34,7 @@ const Header = () => {
       </div>
       <section className="nav-bar">
         <NavLink to="/about" className="nav-link">About Us</NavLink>
-        <NavLink to="/login" className="nav-link"><button className="login-btn">Log In</button></NavLink>
+        {toggleButton()}
       </section>
 
     </header>
