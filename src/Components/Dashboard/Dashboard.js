86be2a74f4f1d7c78bb2377e6e8dde80f87206bf
Regan Losey <regan.losey@gmail.com>
2022-02-13 18:02:55 -0700

    Style dashboard grid

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -77,17 +77,24 @@ const Dashboard = () => {
   } else {
     return (
       <div className="dashboard">
-        <div className="dashboard-content">
+        <div className="dashboard-greeting">
           <h1>Welcome {currentUser.name}</h1>
           <p>Here's an overview of your account</p>
-          <p>You've been invited on a trip! We need a little more info.</p>
-          <Link to="/add"><button>Click here</button></Link>
+        </div>
+        <div className="dashboard-content">
+          <div className="create-invite-wrapper">
           <Link to="/schedule">
-            <button className="create-trip-btn">Create a new trip</button>
+            <button className="dashboard-create-btn">Create a new trip</button>
           </Link>
-          <div className="dashboard-cards">
-            {createTripCards()}
           </div>
+          <div className="dash-cards-wrapper">
+            <h2>Your Trips</h2>
+            <div className="dashboard-cards">
+              {createTripCards()}
+            </div>
+          </div>
+          {/* <p>You've been invited on a trip! We need a little more info.</p>
+          <Link to="/add"><button>Click here</button></Link> */}
         </div>
       </div>
     )
