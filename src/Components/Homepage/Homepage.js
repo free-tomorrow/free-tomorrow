004627468c3db034d07d4c6ca9ef7f41d9c267b2
Delilah Necrason <delilahnecrason@gmail.com>
2022-02-15 17:03:13 -0500

    Write function to conditionally render links on the homepage based on the user being logged in or not

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -13,6 +13,8 @@ const Homepage = () => {
   const url = useLocation().pathname;
   console.log(url)
 
+  const isLoggedIn = localStorage.getItem('savedUser') ? true : false
+
 
   useEffect(() => {
     if(url !== '/') {
@@ -21,6 +23,31 @@ const Homepage = () => {
       console.log(id)
     }
   }, [])
+
+  const redirectIfLoggedIn = () => {
+    const currentUser = localStorage.getItem('savedUser')
+    let homepgLinks;
+    if(currentUser) {
+      const parsedUserId = JSON.parse(currentUser).id;
+      homepgLinks = 
+      <div className="homepg-signup-btn-container">
+        <Link to={`/dashboard/${parsedUserId}`} className="homepg-signup-link" style={{ width: "fit-content" }}>
+          <button className="homepg-signup-btn" >Visit your dashboard</button>
+        </Link>
+      </div>
+    } else {
+      homepgLinks = 
+        <div className="homepg-signup-btn-container">
+        <Link to="/signup" className="homepg-signup-link" style={{ width: "fit-content" }}>
+          <button className="homepg-signup-btn" >Sign up to get started</button>
+        </Link>
+          <p className="homepg-login-link">Already a user? Log in  <Link to="/login" >here</Link></p>
+      </div>
+    }
+  
+
+    return homepgLinks;
+  }
 
 //  useEffect on load to check if there is an id in the current URL
 // if there is an id at the end of the path, grab it and put it in local storage
@@ -33,12 +60,7 @@ const Homepage = () => {
         <h1>Free Tomorrow?</h1>
         <h2>An app that helps you get your travel plans out of the air and onto the ground.</h2>
       </section>
-      <div className="homepg-signup-btn-container">
-        <Link to="/signup" className="homepg-signup-link" style={{ width: "fit-content" }}>
-          <button className="homepg-signup-btn" >Sign up to get started</button>
-        </Link>
-          <p className="homepg-login-link">Already a user? Log in  <Link to="/login" >here</Link></p>
-      </div>
+      {redirectIfLoggedIn()}
       <img src={TravelAliens} className="aliens-img" alt="one eyed aliens" />
       </div>
 
